6208a7c0b802b3b1ddd24cf6116f9764da93dd10
kadir1278 <kadirari@MAU-BIM5>
2021-02-23 09:28:25 +0300

    Dictionary Oluşturuldu Add methodu eklendi

---
+++ MyDictionary/MyDictionary/MyDictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<T> // Bir tip belirt "T"
+    {
+        T[] items;
+        public MyDictionary()
+        {
+            items = new T[0];
+        }
+        public void Add(T item) // "T" tipinde bir item iste
+        {
+            T[] tempArray = items;
+            items = new T[items.Length+1];
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1]=item;
+        }
+        public T[] Items
+        {
+            get { return items; }
+        }
+
+    }
+}
---
+++ MyDictionary/MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int> myDictionary = new MyDictionary<int>();
+            myDictionary.Add(50);
+            myDictionary.Add(25);
+            myDictionary.Add(257);
+            foreach (var item in myDictionary.Items)
+            {
+                Console.WriteLine(item);
+            }
+        }                        
+        
+    }                               
+}                                   
+                                    
